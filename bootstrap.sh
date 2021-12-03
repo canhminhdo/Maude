36f25eec30e068e7641f47e598d000cc8a93f53b
Canh Do Minh <minhcanh99@gmail.com>
2021-12-03 21:38:15 +0900

    Update bootstrap to build with static library

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,10 @@ CFLAGS="-g -Wall -O3 -fno-stack-protector -fstrict-aliasing" \
 CXXFLAGS="-g -Wall -O3 -fno-stack-protector -fstrict-aliasing -std=c++11" \
 CPPFLAGS="-I/Users/Darwin64/include" \
 LDFLAGS="-L/Users/Darwin64/lib" \
+# LIBSIGSEGV_LIB="/Users/Darwin64/lib/libsigsegv.a" \
+# BUDDY_LIB="/Users/Darwin64/lib/libbdd.a" \
+# YICES2_LIB="/Users/Darwin64/lib/libyices.a" \
+# TECLA_LIBS="/Users/Darwin64/lib/libtecla.a /Users/Darwin64/lib/libtecla_r.a -lcurses" \
 GMP_LIBS="/Users/Darwin64/lib/libgmpxx.a /Users/Darwin64/lib/libgmp.a" \
 --prefix="${CWD}/dist"
 
